1fef831241168fe3b663febfb8402ad895170746
Amr Yousry <amryousry19@gmail.com>
2022-08-22 12:03:41 -0400

    Static Classes Exercise One

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Celsius to convert to Fahrenheit: ");
+            int num = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(num));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a Fahrenheit to convert to Celsius: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(num));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    static class TempConverter 
+    {
+        public static double FahrenheitToCelsius(double num) 
+        {
+            double result;
+
+            result = ((num - 32.00) * (5.00 / 9.00));
+            
+            return result;
+        }
+
+        public static double CelsiusToFahrenheit(double num) 
+        {
+            double result;
+
+            result = (num * (9.00 / 5.00)) + 32.00;
+
+            return result;
+        }
+    }
+}
